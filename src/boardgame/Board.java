988e7f6696a9e5ea2e69901d7d702f59b8c6fc46
fernandolacerda1746 <stingeralfa@gmail.com>
2022-12-16 13:07:49 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -69,6 +69,26 @@ public class Board {
 		
 	}
 	
+	public Piece removePiece(Position position) {
+		
+		if(!positionExists(position)) {
+			
+			throw new BoardException("Position not on the board");
+			
+		}
+		
+		if(piece(position) == null) {
+			
+			return null;
+			
+		}
+		
+		Piece aux = piece(position); // Auxiliar obejct of piece for null valor in return
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
+	}
+	
 	private boolean positionExists(int row, int column) {
 		
 		return row >= 0 && row < rows && column >= 0 && column < columns;
